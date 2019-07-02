3d568922803432879967d9cd83b301cc082fee1b
Guillaume Tartara <tguillaume59@gmail.com>
2019-07-02 12:29:39 +0200

    Ajout des dépendances dans le gradle

---
+++ app/src/main/java/com/tguillaume/lamobilery/mobconnect/MBCApplication.kt
@@ -0,0 +1,12 @@
+package com.tguillaume.lamobilery.mobconnect
+
+import android.app.Application
+
+class MBCApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
